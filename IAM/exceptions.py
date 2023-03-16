4049ee8719c1815f5799fad35e97fef5707b1fed
n-xara <najadaxara@gmail.com>
2023-03-16 12:51:57 +0100

    prevent edit or delete for SuperAdmin group

---
+++ IAM/exceptions.py
@@ -0,0 +1,8 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import status, exceptions
+
+
+class NotAllowed(exceptions.APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_detail = _('It is not allowed to modify this resource!')
+    default_code = 'readonly_resource'
--- IAM/views.py
+++ IAM/views.py
@@ -1,8 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, DjangoObjectPermissions
 from .permissions import CustomDjangoModelPermission
-from rest_framework.response import Response
-from rest_framework import status
+from .exceptions import NotAllowed
 from .serializers import *
 from .models import *
 
@@ -18,6 +17,18 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = (IsAuthenticated, CustomDjangoModelPermission, DjangoObjectPermissions)
+
+    def update(self, request, pk=None, *args, **kwargs):
+        instance = self.get_object()
+        if instance and instance.name == "SuperAdmin":
+            raise NotAllowed()
+        return super(GroupViewSet, self).update(request, pk, *args, **kwargs)
+
+    def destroy(self, request, pk=None, *args, **kwargs):
+        instance = self.get_object()
+        if instance and instance.name == "SuperAdmin":
+            raise NotAllowed()
+        return super(GroupViewSet, self).destroy(request, pk, *args, **kwargs)
 
 
 class PermissionViewSet(viewsets.ModelViewSet):
